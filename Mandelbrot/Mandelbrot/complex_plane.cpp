d8bf7773f344a02a68ebeec15c401b47ca19972b
Hobo4rmSpace <154842024+DamianSaelee@users.noreply.github.com>
2024-04-17 17:48:15 -0700

    Completed 1st half. Added where functions should exist in code also.

--- Mandelbrot/Mandelbrot/complex_plane.cpp
+++ Mandelbrot/Mandelbrot/complex_plane.cpp
@@ -21,7 +21,53 @@ void ComplexPlane::updateRender() {
 		for (int y = 0; y < this->m_pixelHeight; y++) {
 			for (int x = 0; x < this->m_pixelWidth; x++) {
 				this->m_vArray[x + y * m_pixelWidth].position = { (float)x, (float)y };
+				// mapPixelToCoords here
+				//countIterations();
+				Uint8 r, g, b = 0;
+				//iterationsToRGB(countIterations(mapPixelToCoords()), r, g, b);
+				this->m_vArray[x + y * m_pixelWidth].color = { r, g, b };
 			}
 		}
+
+		m_state = DISPLAYING;
+	}
+}
+
+size_t ComplexPlane::countIterations(Vector2f coord) {
+	size_t iterations = 0;
+	const double threshold = 2.0;
+
+	double re = coord.x;
+	double im = coord.y;
+
+	complex<double> c(re, im);
+	complex<double> z(0, 0);
+
+	while (abs(z) < threshold && iterations < MAX_ITER) {
+		z = z * z + c;
+		iterations++;
+	}
+
+	return iterations;
+}
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b) {
+	if (count > 54 && count < MAX_ITER){
+		r = 255;
+	}
+	if (count > 44 && count < MAX_ITER) {
+		b = 255;
+	}
+	if (count > 34 && count < MAX_ITER) {
+		r = 0;
+	}
+	if (count > 24 && count < MAX_ITER) {
+		g = 255;
+	}
+	if (count > 14 && count < MAX_ITER) {
+		b = 0;
+	}
+	if (count > 4 && count < MAX_ITER) {
+		r = 255;
 	}
 }
